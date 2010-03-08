ea797d17eae4a06c173c18cf8523753263be3c50
Erlend Nossum <nossum@gmail.com>
2010-03-08 17:50:10 +0000

    Can use standard space functionality.

--- semispace-comet-server/src/main/java/org/semispace/comet/demo/InsertServlet.java
+++ semispace-comet-server/src/main/java/org/semispace/comet/demo/InsertServlet.java
@@ -16,7 +16,7 @@
 
 package org.semispace.comet.demo;
 
-import org.semispace.comet.client.SemiSpaceCometProxy;
+import org.semispace.SemiSpace;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,21 +28,7 @@ import java.io.IOException;
  * Just insert something into the space
  */
 public class InsertServlet extends HttpServlet {
-    private SemiSpaceCometProxy space;
-
-    @Override
-    public void destroy() {
-        super.destroy();
-        space.destroy();
-    }
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        space = new SemiSpaceCometProxy();
-        space.init("http://localhost:8080/semispace-comet-server/cometd/");
-    }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String param = req.getParameter("param");
@@ -52,7 +38,7 @@ public class InsertServlet extends HttpServlet {
         FieldHolder fh = new FieldHolder();
         fh.setFieldA("InsertServlet");
         fh.setFieldB(param);
-        space.write(fh, 15000);
+        SemiSpace.retrieveSpace().write(fh, 15000);
         resp.setContentType("text/plain");
         resp.getWriter().write("Inserted following into space with a lifetime of 15 seconds:\n"+fh);
     }
