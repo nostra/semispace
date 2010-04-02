a0f5169d8d7e73384fdb5bec3fa18a6dbdd019fe
Trygve Lie <post@trygve-lie.com>
2010-04-02 21:25:00 +0000

    Adding code for Erlend to debug nullpointer

--- semispace-comet-server/src/main/webapp/js/core/semispace.Comet.js
+++ semispace-comet-server/src/main/webapp/js/core/semispace.Comet.js
@@ -12,6 +12,26 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *
+ * TODO: Add reload support: http://cometd.org/documentation/cometd/ext/reload
+ *
+ We have:
+ 2010-04-02 03:31:05.716:INFO:/semispace-comet-server:newChannel: /semispace
+ 2010-04-02 03:31:05.716:INFO:/semispace-comet-server:newChannel: /semispace/call
+ 2010-04-02 03:31:05.716:INFO:/semispace-comet-server:newChannel: /semispace/call/read
+ 2010-04-02 03:31:05.716:INFO:/semispace-comet-server:newChannel: /semispace/call/read/*
+
+ 2010-04-02 03:31:05.717:INFO:/semispace-comet-server:newChannel: /semispace/call/take
+ 2010-04-02 03:31:05.717:INFO:/semispace-comet-server:newChannel: /semispace/call/take/*
+
+ 2010-04-02 03:31:05.717:INFO:/semispace-comet-server:newChannel: /semispace/call/write
+ 2010-04-02 03:31:05.717:INFO:/semispace-comet-server:newChannel: /semispace/call/write/*
+
+ 2010-04-02 03:31:05.718:INFO:/semispace-comet-server:newChannel: /semispace/call/notify
+ 2010-04-02 03:31:05.718:INFO:/semispace-comet-server:newChannel: /semispace/call/notify/**
+
+ *
  **/
 
 semispace.Comet = function(connector, server){
@@ -19,7 +39,7 @@ semispace.Comet = function(connector, server){
     var cometd = connector;
     var connected = false;
     var metaListener = undefined;
-
+    var _subscription;
 
     var init = function(){
 
@@ -39,6 +59,33 @@ semispace.Comet = function(connector, server){
             connected = message.successful;
             if (!wasConnected && connected){
                 metaListener(2, message);       // Connected - Server is up
+
+                cometd.batch(function()
+                {
+                    if (_subscription)
+                    {
+                        cometd.unsubscribe(_subscription);
+                    }
+                    var param ="";
+                    _subscription = cometd.subscribe('/semispace/reply/read/*', function(message)
+                    {
+                        var data;
+                        if ( message && message.data && message.data.result) {
+                            data =  message.data.result;
+                            //param = JSON.parse(message.data.result);
+                        } else {
+                            data = "No response from server";
+                        }
+                        // Note - transforming back: JSON.stringify(param)
+                        //dojo.byId('body').innerHTML += '<div>Server Says: <b>'+data+'</b><pre>' + param.org_semispace_comet_demo_FieldHolder.fieldB+ '</pre></div>';
+                        alert('hello 1: ' + data);
+                    });
+
+                    // Waiting an hour, if need be
+                    cometd.publish('/semispace/call/read/1', {duration: '600000', searchMap: {semispaceObjectTypeKey: 'org.semispace.comet.demo.FieldHolder'}});
+
+                });
+
             }else if (wasConnected && !connected){
                 metaListener(3, message);       // Not connected - Server is down
             }else{
@@ -80,6 +127,20 @@ semispace.Comet = function(connector, server){
 
     this.disconnect = function(){
         cometd.disconnect();
+    };
+
+    this.doWrite = function(){
+        // cometd.publish('/semispace/call/write', {duration: '600000', searchMap: {semispaceObjectTypeKey: 'org.semispace.comet.demo.FieldHolder'}});
+
+
+        var m = JSON.stringify({fieldA:'InsertServlet'});
+        cometd.publish('/semispace/call/write/1', {timeToLiveMs: '600000', searchMap: {semispaceObjectTypeKey: 'org.semispace.comet.demo.FieldHolder'}, json: m });
+        //alert('hello');
+    };
+
+    this.doTake = function(){
+        cometd.publish('/semispace/call/read/1', {duration: '600000', searchMap: {semispaceObjectTypeKey: 'org.semispace.comet.demo.FieldHolder'}});
+        //alert('ohh');
     };
 
 };
